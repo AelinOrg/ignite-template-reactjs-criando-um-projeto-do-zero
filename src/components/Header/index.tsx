67a91b3f19940e0df2b38d8d58e24a239158f5e1
marco <marcorezende@outlook.com.br>
2021-12-22 00:39:14 -0300

    update

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,3 +1,17 @@
-export default function Header() {
+import Link from 'next/link';
+import styles from './header.module.scss';
+
+export default function Header(): JSX.Element {
   // TODO
+  return (
+    <header className={styles.container}>
+      <div>
+        <Link href="/">
+          <a>
+            <img src="/images/logo.svg" alt="Logo" />
+          </a>
+        </Link>
+      </div>
+    </header>
+  );
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,11 @@
+import { format } from 'date-fns';
 import { GetStaticProps } from 'next';
+import Head from 'next/head';
+import Link from 'next/link';
 
+import { AiOutlineCalendar } from 'react-icons/ai';
+import { FiUser } from 'react-icons/fi';
+import ptBR from 'date-fns/locale/pt-BR';
 import { getPrismicClient } from '../services/prismic';
 
 import commonStyles from '../styles/common.module.scss';
@@ -24,9 +30,67 @@ interface HomeProps {
   postsPagination: PostPagination;
 }
 
-// export default function Home() {
-//   // TODO
-// }
+export default function Home(): JSX.Element {
+  // TODO
+  const fakePosts: Post[] = [
+    {
+      uid: '123',
+      first_publication_date: '2021-12-22T02:17:19.364Z',
+      data: {
+        author: 'J.K. Rowling',
+        title: 'Como utilizar Hooks',
+        subtitle: 'Pensando em sincronização em vez de ciclos de vida.',
+      },
+    },
+    {
+      uid: '123',
+      first_publication_date: '2021-12-22T02:17:19.364Z',
+      data: {
+        author: 'J.K. Rowling',
+        title: 'Criando um app CRA do zero',
+        subtitle:
+          'Tudo sobre como criar a sua primeira aplicação utilizando Create React App.',
+      },
+    },
+  ];
+
+  return (
+    <>
+      <Head>
+        <title>spacetraveling | Home</title>
+      </Head>
+      <main className={`${commonStyles.commonContainer} ${styles.container}`}>
+        <div>
+          {fakePosts.map(post => (
+            <Link href="#nowhere" key={post.uid}>
+              <a>
+                <h2>{post.data.title}</h2>
+                <p>{post.data.subtitle}</p>
+
+                <div>
+                  <span>
+                    <AiOutlineCalendar strokeWidth="50" size="20" />
+                    {post.data.author}
+                  </span>
+                  <span>
+                    <FiUser strokeWidth="3" size="20" />
+                    {format(
+                      new Date(post.first_publication_date),
+                      'dd LLL yyyy',
+                      {
+                        locale: ptBR,
+                      }
+                    )}
+                  </span>
+                </div>
+              </a>
+            </Link>
+          ))}
+        </div>
+      </main>
+    </>
+  );
+}
 
 // export const getStaticProps = async () => {
 //   // const prismic = getPrismicClient();
